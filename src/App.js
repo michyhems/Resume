de50dd9110b9763bbcd72c442bf27bf0e6e3c782
michyhems <64618905+michyhems@users.noreply.github.com>
2025-11-24 11:47:34 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/layout";
 import Introduction from "./pages/intro";
 import Education from "./pages/education";
