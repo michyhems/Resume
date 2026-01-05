d160a93efeb5de6f751dd0809ac2d8bfa3b866a0
michyhems <64618905+michyhems@users.noreply.github.com>
2026-01-05 14:55:32 +1000

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
     //baseURL: "http://localhost:3500",
-    baseURL: "https://profile-k1lk.onrender.com",
+    baseURL: "https://backend-7vez.onrender.com",
 });
