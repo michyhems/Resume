41b7421670ce868f56339077891a0040ccfb25ea
michyhems <64618905+michyhems@users.noreply.github.com>
2026-02-25 15:15:43 +1000

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
     //baseURL: "http://localhost:3500",
-    baseURL: "https://backend-7vez.onrender.com",
+    baseURL: process.env.SERVER_URL,
 });
